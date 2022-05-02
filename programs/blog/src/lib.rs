b44da446641d0b12a16c417c70d2f5059f58f8df
nicolskiprop <yegonicholas@gmail.com>
2022-05-02 20:11:23 +0300

    fn init blog

--- programs/blog/src/lib.rs
+++ programs/blog/src/lib.rs
@@ -9,7 +9,26 @@ pub mod blog {
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
+
+    pub fn init_blog(ctx: Context<InitBlog>) -> ProgramResult {
+            Ok(())
+    }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+pub struct InitBlog<'info>  {
+    #[account(init, payer = authority, space = 8 + 32 + 32)]
+    pub blog_account: Account<'info, BlogState>,
+    #[account(init, payer = authority, space = 8 + 32 + 32 + 32 + 32 +8)]
+    pub genesis_post_account: Account<'info, PostState>,
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+
+}
+
+#[account]
+pub struct BlogState {
+    pub current_post_key: Pubkey,
+    pub authority: Pubkey,
+}
